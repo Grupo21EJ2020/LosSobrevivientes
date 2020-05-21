cd11195eddf7ea95ca50aebc9d25b7638fe66625
Edith Paola Vidales Velázquez <edith_tigresa10@hotmail.com>
2020-05-21 16:36:38 -0500

    Se agregaron las opciones

---
+++ Menú.py
@@ -0,0 +1,7 @@
+print("Bienvenido ¿Qué es lo que desea hacer?")
+print("1.- Administrar los Empleados\n2.- Administrar Cursos\n" 
+    "3.- Administrar Temas\n4.- Administrar Videos\n"
+    "5.- Administar Temas Asignados al Curso\n6.- Administrar Videos Asignados al Tema"
+    )
+admin = int(input(""))
+print("▒" * 50)
