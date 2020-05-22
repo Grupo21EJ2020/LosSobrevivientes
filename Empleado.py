a063265b024c895bf9f3cfb42a95f571267bd978
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-22 15:38:05 -0500

    Se creo clase Empleado

---
+++ Empleado.py
@@ -0,0 +1,6 @@
+class Empleado:
+    def __init__(self, idEmpleado,nombre, direccion):
+        self.__idEmpleado = idEmpleado
+        self.__nombre = nombre
+        self.__direccion = direccion
+        
