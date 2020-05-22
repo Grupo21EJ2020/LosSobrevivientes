868f3ed0a22b7e3b1cb9db1d040494ac3a95e0dd
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-22 15:54:16 -0500

    add setters a clase Empleado

--- Empleado.py
+++ Empleado.py
@@ -4,14 +4,29 @@ class Empleado:
         self.__nombre = nombre
         self.__direccion = direccion
 
+
 @property
 def idEmpleado (self):
     return self.__idEmpleado
+
+@idEmpleado.setter
+def idEmpleado (self,valor):
+    self.__idEmpleado = valor 
+
 
 @property
 def nombre (self):
     return self.__nombre
 
+@nombre.setter
+def nombre (self,valor):
+    self.__nombre = valor
+
+
 @property
 def direccion (self):
-    return self.__direccion                
+    return self.__direccion  
+
+@direccion.setter
+def direccion (self,valor):
+    return self.__direccion = valor                   
