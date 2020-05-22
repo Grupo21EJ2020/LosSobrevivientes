d6e828b4b86990ac119e1e19b52094b5dafd7961
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-22 15:48:44 -0500

    add getters a clase Empleado

--- Empleado.py
+++ Empleado.py
@@ -3,4 +3,15 @@ class Empleado:
         self.__idEmpleado = idEmpleado
         self.__nombre = nombre
         self.__direccion = direccion
-        
+
+@property
+def idEmpleado (self):
+    return self.__idEmpleado
+
+@property
+def nombre (self):
+    return self.__nombre
+
+@property
+def direccion (self):
+    return self.__direccion                
