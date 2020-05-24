5384f462cf65adec496d2581a5f77d5d51027545
Edith Paola Vidales Velázquez <edith_tigresa10@hotmail.com>
2020-05-24 03:41:26 -0500

    Se rrealizó la función eliminarInfo con exito

--- ClaseTema.py
+++ ClaseTema.py
@@ -19,11 +19,14 @@ class Tema():
         archivo.close()
 
     def eliminarInfo(self):
-        archivo = open("./archivos/Temas.txt", "r+")
+        archivo = open("./archivos/Temas.txt","r",encoding ='utf8')
+        guardTemp = []
         for renglon in archivo:
             datos = renglon.split("\n")
-            if datos[0] == (self.__idTema + "|" + self.__nombre):
-                archivo.write("")
-            else:
-                print("No existe este dato")
+            if datos[0] != (self.__idTema + "|" + self.__nombre):
+                guardTemp.append(datos[0])
+                archivo2 = open("./archivos/Temas.txt","w",encoding = "utf8")
+                for i in guardTemp:
+                    archivo2.write(i + "\n")
+                archivo2.close()
         archivo.close()
