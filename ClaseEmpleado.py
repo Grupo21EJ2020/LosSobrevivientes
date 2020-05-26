32272d1621d970713398d09a1ff8cc6621519a04
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-26 15:19:47 -0500

    modifique nombre al archivo Empleado.py a ClaseEmpleado.py

---
+++ ClaseEmpleado.py
@@ -0,0 +1,41 @@
+class Empleado:
+    def __init__(self, idEmpleado,nombre, direccion):
+        self.__idEmpleado = idEmpleado
+        self.__nombre = nombre
+        self.__direccion = direccion
+
+    @property
+    def idEmpleado (self):
+        return self.__idEmpleado
+
+    @idEmpleado.setter
+    def idEmpleado (self,valor):
+        self.__idEmpleado = valor 
+
+
+    @property
+    def nombre (self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre (self,valor):
+        self.__nombre = valor
+
+
+    @property
+    def direccion (self):
+        return self.__direccion  
+
+    @direccion.setter
+    def direccion (self,valor):
+        return self.__direccion = valor 
+
+
+    def agregarInfo(self):
+        archivo = open("./archivos/Empleados.txt","a",encoding='utf8')
+
+        archivo.write(self.__idEmpleado "\n" + self.__nombre + "\n" + self.__direccion)
+
+        archivo.close()
+
+           
