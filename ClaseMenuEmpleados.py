9c613ad707853dc05dd50172f9776d21a785c7b2
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-26 16:35:29 -0500

    add menu y opcion 1 a ClaseMenuEmpleado

--- ClaseMenuEmpleados.py
+++ ClaseMenuEmpleados.py
@@ -7,8 +7,18 @@ class MenuEmpleado():
         while opcion < 6 :
             print ("¿Que accion se desea realizar?")
             print("1.Agregar Info\n 2.Borrar Info\n 3.Modificar info\n 4.Consultar Info\n 5.Ver detalles especificos\n 6.Regresar al menu" )
-            opcion = int(input(""))
+            opcion = int(input("Elige una opcion: "))
+            print ("*" * 20)
+            if opcion == 1:
+                idEmpleado = input("Ingrese el Id del Empleado: ")
+                nombre = input("Ingrese el nombre del Empleado: ")   
+                direccion = input("Ingrese la direccion del Empleado: ")
+                agregar = Empleado(idEmpleado, nombre, direccion)
+                agregar.agregarInfo()           
+   
+                print("Se ha agregador la información correctamente")
+                print("*" * 20)
 
-            if opcion == 6:
+            elif opcion == 6:
                 print ("Regresaras al menu principal ")
-        return        
+             
