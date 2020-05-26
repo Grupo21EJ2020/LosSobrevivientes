8c7a04370026adc72696d19efe1701694f1ded04
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-25 21:46:47 -0500

    corregi error

--- Menú.py
+++ Menú.py
@@ -17,7 +17,7 @@ while finish > 7:
         opcion = 0
         while opcion < 6 :
             print ("¿Que accion se desea realizar?")
-            print("1.Agregar Info\n 2.Borrar Info\n 3.Modificar info\n 4.Consultar Info\n 5.Ver detalles especificos\ 6.Regresar al menu" )
+            print("1.Agregar Info\n 2.Borrar Info\n 3.Modificar info\n 4.Consultar Info\n 5.Ver detalles especificos\n 6.Regresar al menu" )
             opcion = int(input(""))
 
             if opcion == 6:
