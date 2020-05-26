a42b781ab2857d20bede200b03e8e14d72651259
Edith Paola Vidales Velázquez <edith_tigresa10@hotmail.com>
2020-05-25 22:53:23 -0500

    Se creó una cClaseMenÃuTemas.py para ocupar menos espacio en el menú

--- ClaseMenuTemas.py
+++ ClaseMenuTemas.py
@@ -1,31 +1,8 @@
 from ClaseTema import Tema
 
-finish = 8
-while finish > 7:
+class MenuTema():
 
-    print("Bienvenido ¿Qué es lo que desea hacer?")
-    print("1.- Administrar los Empleados\n2.- Administrar Cursos\n" 
-        "3.- Administrar Temas\n4.- Administrar Videos\n"
-        "5.- Administar Temas Asignados al Curso\n6.- Administrar Videos Asignados al Tema\n"
-        "7.- Salir"
-        )
-    admin = int(input(""))
-    print("▒" * 50)
-
-    
-    if admin == 1:
-        opcion = 0
-        while opcion < 6 :
-            print ("¿Que accion se desea realizar?")
-            print("1.Agregar Info\n 2.Borrar Info\n 3.Modificar info\n 4.Consultar Info\n 5.Ver detalles especificos\n 6.Regresar al menu" )
-            opcion = int(input(""))
-
-            if opcion == 6:
-                print ("Regresaras al menu principal ")
-            
-
-
-    elif admin == 3:
+    def menu(self):
         ciclo = 6
         while ciclo > 5:
             print("¿Que desea realizar?")
@@ -74,6 +51,4 @@ while finish > 7:
                 print("▒" * 50)
             elif rea == 5:
                 ciclo = 5
-
-    elif admin == 7:
-        finish = 7
+        return
