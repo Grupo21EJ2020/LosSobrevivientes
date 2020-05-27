fc21e093864b020e1882fb76e10bba0f28070439
Alejandra Anahi Correa Aldape <alecorrea0752@gmail.com>
2020-05-26 20:28:37 -0500

    add modificar info y borrar info a ClaseEmpleado.py

--- ClaseEmpleado.py
+++ ClaseEmpleado.py
@@ -31,7 +31,7 @@ class Empleado:
         self.__direccion = valor   
 
   
-  
+
     def agregarInfo(self):
         archivo = open("./archivos/Empleados.txt","a",encoding='utf8')
 
@@ -39,4 +39,46 @@ class Empleado:
 
         archivo.close()
 
-           
+    def borrarInfo (self):
+        archivo = open("./archivos/Empleados.txt","r",encoding ='utf8')
+        lista = []
+        for x in archivo:
+            datos = x.split("\n")
+            if datos[0] != (self.__idEmpleado + "|" + self.__nombre + "|" + self.__direccion):
+                lista.append(datos[0])
+                archivo2 = open("./archivos/Empleados.txt","w",encoding = "utf8")
+                for i in lista:
+                    archivo2.write(i + "\n")
+                archivo2.close()
+        archivo.close()
+
+    
+
+    def modificarInfo (self):
+        archivo = open("./archivos/Empleados.txt","a",encoding='utf8')
+        cam1 = []
+        cam = []
+        guardar = archivo.readlines()
+        for gard in guardar:
+            gard = gard.replace("\n", "")
+            cam1.append(gard)
+        for renglon in cam1:
+            datos = renglon.split("|")
+            if datos[0] == (self.__idEmpleado,):
+                datosNuevos = datos[1].replace(datos[1], self.__nombre + "|" + self.__direccion + "\n")
+                datosCambiados = (datos[0] + "|" + datosNuevos)
+                cam.append(datosCambiados)
+            else:
+                datos = (renglon + "\n")
+                cam.append(datos)
+        archivos2 = open("./archivos/Empleados.txt","w",encoding = "utf8")                
+        for c in cam:
+            cambios = c
+            archivos2.write(cambios)       
+        archivos2.close()
+        archivo.close()
+
+
+
+
+             
